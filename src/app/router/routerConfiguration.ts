4617d4585c6f6af6e06e13a916e63547b314f35e
Anton Liubimov <ant.liubimov@gmail.com>
2023-10-07 12:33:50 +0500

    fix: добавить страницу event в роутер

---
+++ src/app/router/routerConfiguration.ts
@@ -0,0 +1,56 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import {RouteConfig} from "vue-router/types/router";
+import {EventsPage} from "../pages/eventsPage";
+import {EventPage} from "../pages/eventPage";
+
+Vue.use(VueRouter);
+
+/**
+ * Класс отвечающий за создание роутингов и инициализацию роутера
+ */
+export class RouterConfiguration {
+
+    /** Экземпляр роутера */
+    private static router: VueRouter;
+
+    /**
+     * Возвращает инициализированный экземпляр роутера
+     * @returns {VueRouter} роутер
+     */
+    static getRouter(): VueRouter {
+        if (!RouterConfiguration.router) {
+            RouterConfiguration.router = new VueRouter({
+                base: "/",
+                routes: RouterConfiguration.createRoutes(),
+                scrollBehavior: ((): any => ({x: 0, y: 0}))
+            });
+        }
+        return RouterConfiguration.router;
+    }
+
+    private static createRoutes(): RouteConfig[] {
+        return [
+            {
+                path: "*",
+                redirect: "/events"
+            },
+            {
+                name: "events",
+                path: "/events",
+                component: EventsPage,
+                meta: {
+                    title: "События",
+                }
+            },
+            {
+                name: "event",
+                path: "/event",
+                component: EventPage,
+                meta: {
+                    title: "Событие",
+                },
+            }
+        ];
+    }
+}
